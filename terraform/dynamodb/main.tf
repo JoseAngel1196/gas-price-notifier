a8d7b741545a6513beeabe72b450cae95b6bbdcc
Jose Hidalgo <joseangel1196@hotmail.com>
2022-03-21 22:40:04 -0400

    progress

--- terraform/dynamodb/main.tf
+++ terraform/dynamodb/main.tf
@@ -1,17 +1,11 @@
 resource "aws_dynamodb_table" "gasoline_prices_table" {
   name           = "GasolinePrices"
-  hash_key       = "gasolinePriceId"
-  range_key      = "createdAt"
+  hash_key       = "publishedAt"
   read_capacity  = 20
   write_capacity = 20
 
   attribute {
-    name = "towns"
-    type = "S"
-  }
-
-  attribute {
-    name = "price"
+    name = "publishedAt"
     type = "S"
   }
 }
--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -13,3 +13,26 @@ resource "aws_s3_bucket_public_access_block" "deployment_bucket_public_policy" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "Price-Notifier-role"
+  assume_role_policy = jsonencode(
+    {
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
+          Principal = {
+            Service = "lambda.amazonaws.com"
+          }
+        },
+      ]
+      Version = "2012-10-17"
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach_policy" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.iam_role_policy_for_lambda.arn
+}
